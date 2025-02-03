b820239fc42e775a854ae18b3ce000bdff598a05
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-02-03 18:31:01 +0000

    Data Type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+ "use strict"//treat all js as newer Version
+
+ let name="Dipchand"
+ let age=17
+ let isLoggedIn=false
+
+ //primitive dataTypes
+
+ //number=> 2 to power 53
+ //bigint
+ //string=>""
+ //boolean=> true/false
+ //null => standalone value
+ //undefined => value is not assigned
+ //symbol =>uniqueness
+
+
+ //object
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object 
