8c01b22d5067c2e632caf254f683c33af9f4cec1
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-02-03 18:09:48 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=12345
+let accountEmail="dipchandkalwar@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=2 // not allowed
+
+ accountEmail="dip@gmail.com"
+ accountPassword="2121"
+ accountCity="kathmandu"
+
+
+//console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
