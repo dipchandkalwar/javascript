6637eafc0c0ecfc771a9ce1f4df2f29fdb0b0029
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-02-04 04:27:08 +0000

    comparisin of datatype

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+//basic compariosn
+// console.log(2==2)
+// console.log(2<=2)
+// console.log(2>=2)
+// console.log(2!=2)
+
+//Not used this type of comapriosn
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
