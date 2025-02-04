90e6bc1de2f846fd4024890c1523bc92f31ecee6
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-02-04 03:45:09 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+// let score=33
+// console.log(typeof(score))
+
+// let scores="33abc"
+// console.log(typeof(scores))
+
+// let valueNumber=Number(scores)
+// console.log(typeof(valueNumber))
+// console.log(valueNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 false=>0
+
+// let isLoggedIn=1;
+ 
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1=> true; 0=>false
+//""=>false
+//"dipchand"=>true
+
+let somenumber=33
+let stringNumber=String(somenumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
