640ffbde0e1b7c3f2d7d6aba395eb75af057c7b1
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-02-05 03:58:03 +0000

    strings Method

---
+++ 01_basics/05_string.js
@@ -0,0 +1,23 @@
+const name="dip"
+const repoCount=80
+
+//console.log(name + repoCount + "Value");
+
+//console.log(`My name is ${name} and my count is${repoCount}`);
+
+const gameName=new String('dipchancd');
+// console.log(gameName[0]);
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('c'));
+
+const word="   hello   "
+// console.log(word);
+// console.log(word.trim());
+
+const word1=" dipchand-kalwar-20"
+console.log(word1.split('-'));
