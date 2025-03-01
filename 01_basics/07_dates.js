cf2c94018e7ef3c728c19dcc812821a5f089eb8d
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-03-01 09:23:44 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,4 +1,20 @@
 //Dates
 
 let myDate=new Date()
-console.log(myDate.toString());
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate=new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate=new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleString()); 
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
