4af08765058a9c95af4205fdfd6c7809735a5a01
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-03-03 04:46:36 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,33 @@
+
+//Function
+
+function f1(){
+    console.log("D");
+    console.log("I");
+    console.log("p");
+}
+
+// f1();
+
+// function addTwoNumbers(n1,n2){
+//     console.log(n1+n2);
+// }
+
+
+function addTwoNumbers(n1,n2){
+        let result=n1+n2;
+        return result;
+ }
+    
+const result=addTwoNumbers(2,3);
+// console.log(result);
+
+function loginUseMessage(username){
+    if(username===undefined){
+        console.log("please enter user name");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUseMessage("dip"));
