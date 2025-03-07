096abb2f87d3de77f76c6fb27e7e9c7c5fbb810b
Dipchand kalwar <111203713+dipchandkalwar@users.noreply.github.com>
2025-03-07 05:07:10 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,48 @@
+const user={
+    username:"hgiteh",
+    price:999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username},welcome to website`);
+
+        //console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username="dip"
+// user.welcomeMessage();
+
+//console.log(this);
+
+// function chai(){
+//     let username="dip"
+//     console.log(this.username);
+// }
+//  chai();
+
+
+
+// const chai=function() {
+//         let username="dip"
+//         console.log(this.username);
+//     }
+//      chai();
+
+// const chai= () =>{
+//         let username="dip"
+//         console.log(this);
+//     }
+//      chai(); 
+
+// const addTwo=(num1,num2)=>{
+//     return num1+num2;
+// }
+
+// const addYwo=(num1,num2)=> num1+num2;
+
+// const addYwo=(num1,num2)=> (num1+num2);
+
+//return object
+
+// const addYwo=(num1,num2)=> ({username:"hitesh"});
